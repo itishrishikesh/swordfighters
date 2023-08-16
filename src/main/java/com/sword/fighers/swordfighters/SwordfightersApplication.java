66db7b65f8548df384db65209974031ccb39254b
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-08-17 01:06:18 +0530

    Initial commit

---
+++ src/main/java/com/sword/fighers/swordfighters/SwordfightersApplication.java
@@ -0,0 +1,13 @@
+package com.sword.fighers.swordfighters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordfightersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordfightersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sword/fighers/swordfighters/SwordfightersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sword.fighers.swordfighters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordfightersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
