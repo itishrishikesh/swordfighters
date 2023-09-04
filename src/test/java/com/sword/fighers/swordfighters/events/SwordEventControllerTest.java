1950a07a15c10b4026bb5aa7dc7e86340f233f76
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-09-05 01:01:34 +0530

    weird test template

---
+++ src/test/java/com/sword/fighers/swordfighters/events/SwordEventControllerTest.java
@@ -0,0 +1,32 @@
+package com.sword.fighers.swordfighters.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwordEventControllerTest {
+
+    @Test
+    void events() {
+    }
+
+    @Test
+    void showAddEventForm() {
+    }
+
+    @Test
+    void addEvent() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void updateEventPost() {
+    }
+}
